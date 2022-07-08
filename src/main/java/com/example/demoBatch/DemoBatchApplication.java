29535c699ed1f8ea6e385be352793f55f24fe7ef
ThiagoRocha1991 <37739685+ThiagoRocha1991@users.noreply.github.com>
2022-07-08 20:56:00 -0300

    first commit

---
+++ src/main/java/com/example/demoBatch/DemoBatchApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBatchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/demoBatch/DemoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
